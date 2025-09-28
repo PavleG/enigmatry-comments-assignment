2943b0b7244a4fa86d23bc23b78e9c7f4eea746a
PavleG <pavlemgavrilovic@gmail.com>
2025-09-29 01:02:38 +0200

    Added search field

--- enigmatry-entry-blueprint-app/src/app/shared/components/header/header.component.ts
+++ enigmatry-entry-blueprint-app/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { MaterialModule } from '@shared/material.module';
 import { Language, setCurrentLanguage } from 'src/i18n/language';
@@ -7,11 +8,12 @@ import { Language, setCurrentLanguage } from 'src/i18n/language';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, MaterialModule, CommonModule],
+  imports: [RouterLink, MaterialModule, CommonModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  searchTerm = '';
   languages = [
     {
       description: 'English',
@@ -32,6 +34,12 @@ export class HeaderComponent {
       short: 'sr'
     }
   ];
+
+  onSearch(term: string) {
+    // eslint-disable-next-line no-console
+    console.log('Search term:', term);
+    // TODO: Inject comments service
+  }
 
   onLanguageSelect(lang: string) {
     setCurrentLanguage(lang as Language);
